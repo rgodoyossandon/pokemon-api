ed050faf987f85326c72225693a0988239c60308
Rodrigo Godoy <rgodoyossandon@gmail.com>
2022-12-17 16:01:33 -0300

    Api consumida

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+//Componets
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Button } from "./components/Button";
+import { Card } from "./components/Card";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+//Styles
+import './sass/App.scss';
+
+//Icons
+import { TiArrowLeftOutline } from "react-icons/ti";
+import { TiArrowRightOutline } from "react-icons/ti";
+
+//Hooks
+import { useState, useEffect } from "react";
+
+
+const App = () => {
+
+    const [pokemonId, setPokemonId] = useState(1);
+    const [pokemonEvolutions, setPokemonEvolutions] = useState([])
+
+    useEffect(() => { getEvolution(pokemonId) }, [pokemonId])
+
+    async function getEvolution(id) {
+        const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`)
+        const data = await response.json()
+
+        let pokemonEvoArray = []
+
+        let pokemonLv1 = data.chain.species.name
+        let pokemonLv1Img = await getPokemonImgs(pokemonLv1)
+        pokemonEvoArray.push([pokemonLv1, pokemonLv1Img])
+
+
+        if (data.chain.evolves_to.lenght !== 0) {
+            let pokemonLv2 = data.chain.evolves_to[0].species.name;
+            let pokemonLv2Img = await getPokemonImgs(pokemonLv2)
+            pokemonEvoArray.push([pokemonLv2, pokemonLv2Img])
+
+            if (data.chain.evolves_to[0].evolves_to !== 0) {
+                let pokemonLv3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+                let pokemonLv3Img = await getPokemonImgs(pokemonLv3);
+                pokemonEvoArray.push([pokemonLv3, pokemonLv3Img])
+                
+            }
+
+    }
+        setPokemonEvolutions(pokemonEvoArray);    
 }
 
-export default App
+
+async function getPokemonImgs(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+    const data = await response.json()
+    return data.sprites.other['official-artwork'].front_default;
+}
+//Functions button
+const prevClick = () => { (pokemonId === 1) ? setPokemonId(1) : setPokemonId(pokemonId - 1) };
+const nextClick = () => { setPokemonId(pokemonId + 1) };
+
+return (
+
+    <div className="app">
+        {/*Cards*/}
+        <div className="card_container">
+            {pokemonEvolutions.map(pokemon =>
+                <Card
+                    key={pokemon[0]}
+                    name={pokemon[0]}
+                    img={pokemon[1]}
+                />)}
+        </div>
+
+        {/*Button*/}
+        <div className="buttons_container">
+            <Button icon={<TiArrowLeftOutline />} handleClick={prevClick} />
+
+
+
+            <Button icon={<TiArrowRightOutline />} handleClick={nextClick} />
+        </div>
+
+    </div>
+
+)
+}
+
+export { App }
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+
+import '../sass/Card.scss';
+
+const Card = ({name, img}) => {
+
+    return (
+
+        <div className="card">
+            <div className="card__circle"></div>
+            <p className="card__name">{name}</p>
+            <img className="card__img" src={img} alt="pokemon_img" />
+
+        </div>
+
+    )
+}
+export { Card }
+
